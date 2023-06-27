85351b8f994960a67e25facef37c28b1db550d3d
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2023-06-27 23:00:05 +0300

    Rebuilded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
-import user from './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
-import { Titles } from './Titles/Titles.styled';
-import { Profile } from './Profile/User';
+import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
-console.log(friends);
+// import { Title } from './App.styled';
 
 export const App = () => {
   return (
     <>
-      <Titles>Task 1:</Titles>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -20,12 +20,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Titles>Task 2:</Titles>
       <Statistics title="Upload stats" stats={data} />
-      <Titles>Task 3:</Titles>
-      <FriendList friends = {friends}/>
-      <Titles>Task 4:</Titles>
-      <Titles>Game Over!</Titles>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
