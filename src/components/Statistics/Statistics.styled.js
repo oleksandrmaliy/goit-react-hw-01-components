7122f1c5768e0f91dc59772b5813d22358c89635
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2023-06-27 23:06:44 +0300

    27

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -14,6 +14,7 @@ export const Item = styled.li`
   display: flex;
   flex-direction: column;
   background-color: lightgray;
+  border: 1px solid white;
   padding: 10px 15px;
   width: calc((100% - 20px)/ 5);
 `;
